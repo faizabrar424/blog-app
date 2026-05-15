2ad9c5f73361d50f93bbc5aee6f0359fc1f64b0e
faizabrar424 <faizabrar424@gmail.com>
2026-05-15 18:49:00 +0700

    fix: add JWT expiry

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,8 +1,8 @@
-import User from "@/server/model/User";
-import bcrypt from "bcryptjs";
-import { NextRequest, NextResponse } from "next/server";
-import z from "zod";
-import * as jose from "jose";
+import User from '@/server/model/User';
+import bcrypt from 'bcryptjs';
+import { NextRequest, NextResponse } from 'next/server';
+import z from 'zod';
+import * as jose from 'jose';
 
 const loginSchema = z.object({
   email: z.string().email(),
@@ -19,25 +19,25 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: `${String(error.path[0])} - ${error.message}` }, { status: 400 });
     }
 
-    const user = await User.where("email", body.email).first();
-    if (!user) return NextResponse.json({ message: "Invalid email/password" }, { status: 401 });
+    const user = await User.where('email', body.email).first();
+    if (!user) return NextResponse.json({ message: 'Invalid email/password' }, { status: 401 });
 
     // const password = bcrypt.compareSync(body.password, user.password);
     const password = await bcrypt.compare(body.password, user.password);
-    if (!password) return NextResponse.json({ message: "Invalid email/password" }, { status: 401 });
+    if (!password) return NextResponse.json({ message: 'Invalid email/password' }, { status: 401 });
 
     const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    const alg = "HS256";
+    const alg = 'HS256';
 
     const token = await new jose.SignJWT({ _id: user._id.toString(), name: user.name })
       .setProtectedHeader({ alg })
       .setIssuedAt()
-      //   .setExpirationTime("1h")
+      .setExpirationTime('1h')
       .sign(secret);
 
     return NextResponse.json({ token, _id: user._id }, { status: 200 });
   } catch (err) {
-    console.log(err, "<<<<<<<<<<<");
-    return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+    console.log(err, '<<<<<<<<<<<');
+    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
   }
 }
