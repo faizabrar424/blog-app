f87fca45a277cacf2607d7f17af95860a9f87abb
Faiz Abrar <faizabrar424@gmail.com>
2026-05-19 16:49:36 +0700

    fix: add allowedOrigins for reverse proxy

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:8080', 'localhost:3000']
+    }
+  }
 };
 
 export default nextConfig;
